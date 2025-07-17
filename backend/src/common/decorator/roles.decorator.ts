80e54b0956ce11b4e931d4747a343d992161f974
ThuanNTV <thuanntv721@gmail.com>
2025-07-18 04:45:23 +0700

    clean code

--- backend/src/common/decorator/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from 'src/dto/dtoUsers/create-user.dto';
-
-export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
